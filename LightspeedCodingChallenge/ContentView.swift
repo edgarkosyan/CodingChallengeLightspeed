0b02150b00ebd717075d527230ad40610d7031f2
edgar kosyan <edgarkosyan@MPM3.local>
2024-11-04 10:47:18 +0400

    commit all initial files

---
+++ LightspeedCodingChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LightspeedCodingChallenge
+//
+//  Created by edgar kosyan on 04.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LightspeedCodingChallenge/LightspeedCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightspeedCodingChallengeApp.swift
+//  LightspeedCodingChallenge
+//
+//  Created by edgar kosyan on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct LightspeedCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
